8aa413126a12339d5406e902afa0ea21c4118c76
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-02 11:40:48 -0300

    Update project files

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
-const app = document.createElement('div')
-document.body.append(app)
-document.body.style.margin = '1rem'
-
 const state = { nums: [], wins: [] }
 
-const title = document.createElement('div')
-title.innerText = 'Lottery'
-title.style.marginBottom = '1rem'
-app.append(title)
+const txtNumber = document.getElementById('txtNumber')
 
-const inputNumber = document.createElement('input')
-inputNumber.type = 'number'
-inputNumber.style.marginBottom = '1rem'
-app.append(inputNumber)
-
-const btnSave = document.createElement('button')
-btnSave.innerText = 'save'
+const btnSave = document.getElementById('btnSave')
 btnSave.addEventListener('click', () => {
-  state.nums = Array.from(Array(+inputNumber.value)).map((_, i) => i + 1)
+  console.log('btnSave click')
+  state.nums = Array.from(Array(+txtNumber.value)).map((_, i) => i + 1)
 })
-btnSave.style.marginBottom = '1rem'
-app.append(btnSave)
 
-const btnRandom = document.createElement('button')
-btnRandom.innerText = 'random'
+const txtResult = document.getElementById('txtResult')
+
+const btnRandom = document.getElementById('btnRandom')
 btnRandom.addEventListener('click', () => {
-  if (state.nums.length == state.wins.length) {
-    alert('all numbers')
-    return
-  }
+  console.log('btnRandom click')
+  if (state.nums.length == state.wins.length)
+    return alert('all numbers')
+  txtResult.innerText = randomNumber()
+})
+
+const randomNumber = () => {
   let index = -1
   do index = Math.floor(Math.random() * state.nums.length)
   while (state.wins.indexOf(index) != -1)
   state.wins.push(index)
-  txtNumber.innerText = state.nums.at(index)
-})
-btnRandom.style.marginBottom = '1rem'
-app.append(btnRandom)
-
-const txtNumber = document.createElement('div')
-txtNumber.style.marginBottom = '1rem'
-app.append(txtNumber)
+  return state.nums.at(index)
+}
