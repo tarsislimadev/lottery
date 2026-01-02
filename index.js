759309d3bf78f8b9e911877b0ada9ed709a31f7e
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-02 11:59:04 -0300

    Update project files

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ const txtNumber = document.getElementById('txtNumber')
 
 const btnSave = document.getElementById('btnSave')
 btnSave.addEventListener('click', () => {
-  console.log('btnSave click')
+  txtNumber.disabled = true
+  btnSave.disabled = true
+  btnRandom.disabled = false
+  txtResult.innerText = ''
   state.nums = Array.from(Array(+txtNumber.value)).map((_, i) => i + 1)
+  state.wins = []
 })
 
 const txtResult = document.getElementById('txtResult')
 
 const btnRandom = document.getElementById('btnRandom')
 btnRandom.addEventListener('click', () => {
-  console.log('btnRandom click')
-  if (state.nums.length == state.wins.length)
-    return alert('all numbers')
+  if (state.nums.length == state.wins.length) {
+    txtNumber.disabled = false
+    btnSave.disabled = false
+    btnRandom.disabled = true
+    txtResult.innerText = 'ended'
+    return
+  }
   txtResult.innerText = randomNumber()
 })
 
